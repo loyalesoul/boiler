55898f4aef35d788b3e5dad6915896832d88aaf9
Loyale <thanhtt.1393@gmail.com>
2024-04-09 09:40:26 +0000

    Update pre-commit

--- boiler/main.py
+++ boiler/main.py
@@ -3,10 +3,10 @@ import logging
 from datetime import datetime
 from module.module import Module
 
+
 def setup_logging(logging_level=logging.DEBUG):
-    # Set up logging
-    main_logger = logging.getLogger()
-    main_logger.setLevel(logging_level)
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging_level)
 
     # Set up a stream handler to log to the console
     console_handler = logging.StreamHandler()
@@ -15,10 +15,9 @@ def setup_logging(logging_level=logging.DEBUG):
         "%(asctime)s %(filename)s:%(lineno)s %(levelname)s p:%(processName)s t:%(threadName)s: %(message)s"
     )
     console_handler.setFormatter(formatter)
-    main_logger.addHandler(console_handler)
+    root_logger.addHandler(console_handler)
 
-    # Create log directory if it doesn't exist
-    log_directory = './logs'
+    log_directory = "./logs"
     os.makedirs(log_directory, exist_ok=True)
 
     # Set up a file handler to log to a file with name based on the date
@@ -27,15 +26,16 @@ def setup_logging(logging_level=logging.DEBUG):
     file_handler = logging.FileHandler(log_file_path)
     file_handler.setLevel(logging_level)
     file_handler.setFormatter(formatter)
-    main_logger.addHandler(file_handler)
+    root_logger.addHandler(file_handler)
+
 
 def main():
-    # Configure logging
     setup_logging()
 
-    # Your main logic
-    module = Module()
+    m = Module()
+    m.do_stuff()
     logging.info("Log in main")
+
 
 if __name__ == "__main__":
     main()
